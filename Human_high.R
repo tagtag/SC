aa989faa9fa785fd3cc049f724e424bdcfbec418
Y-h. Taguchi <tag@granular.com>
2018-03-17 19:52:14 +0900

    Create Human_high.R

---
+++ Human_high.R
@@ -0,0 +1,13 @@
+x <- read.csv("GSE76381_EmbryoMoleculeCounts.cef.txt.gz",sep="\t",skip=1)
+x <- x[-c(1:3),]
+x <- x[,-2]
+x[,-1] <- apply(x[,-1],2,as.numeric)
+require(locfit)
+Var <- apply(x[,-1],1,var)
+Mean <- rowMeans(x[,-1])
+Y <- Var^0.5/Mean
+NLS <- nls(log10(Y)~0.5*log10(a/Mean+b),start=list(a=1,b=1),trace=T)
+P <- pchisq(scale(log10(Y) - predict(NLS,Mean))^2,1,lower.tail=F) #human
+P <- pchisq(scale(log10(Y[!is.na(Y)]) - predict(NLS,Mean))^2,1,lower.tail=F) #mouse
+x_human_high <- x[!is.na(Y),][p.adjust(P,"BH")<0.01,]
+#x_human_high includes highly variables genes for human. The first column is gene names.
